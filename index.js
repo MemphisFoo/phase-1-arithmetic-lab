fb0580f3c22fda692fba15b056ecddf179b24fe9
Christian Trublet de Nermont <cjwtdn@gmail.com>
2022-07-03 15:12:26 -0600

    commit tree c8cfee03e6ff92e1308214a9db9ee49cd33788fb

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 1);
+
+let num3 = 25;
+let num4 = 7;
+
+let mod = num3 % num4;
+
+let max; 
+max = Math.max(5, 10, 15, 20);
